5a70a116107a07b7073ab35071d2779ae08d6c0b
Letícia Ferreira Pinto <103539215+L3ticialele@users.noreply.github.com>
2023-10-30 18:15:43 -0300

    Add files via upload

---
+++ Altomare/view-jsp/src/main/java/br/cefetmg/inf/sisloc/controller/GravarInsercaoFuncionario.java
@@ -0,0 +1,40 @@
+
+package br.cefetmg.inf.sisloc.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import br.cefetmg.altomare.model.dto.Funcionario;
+import br.cefetmg.altomare.model.service.IManterFuncionario;
+import br.cefetmg.altomare.model.service.ManterFuncionario;
+
+public class GravarInsercaoFuncionario {
+    
+    public static String execute(HttpServletRequest request){
+        String jsp;
+        try{
+            String nome = request.getParameter("nomeFuncionario");
+            String email = request.getParameter("emailFuncionario");
+            String senha = request.getParameter("senhaFuncionario");
+            
+            Funcionario funcionario = new Funcionario();
+            funcionario.setNome(nome);
+            funcionario.setEmail(email);
+            funcionario.setSenha(senha);
+            
+            IManterFuncionario manterFuncionario = new ManterFuncionario();
+            Long funcionarioCpf =  manterFuncionario.cadastrar(funcionario);
+            
+            if(funcionarioCpf != null){
+                jsp = ListarFuncionario.execute(request);
+            }else{
+                String erro = "Ocorreu um erro ao gravar esse registro.";
+                request.setAttribute("erro", erro);
+                jsp = "/erro.jsp";
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+    
+}
