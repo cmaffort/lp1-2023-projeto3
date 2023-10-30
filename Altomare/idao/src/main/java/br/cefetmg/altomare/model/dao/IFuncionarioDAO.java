157178ef4e9facf3f02d7b894e8eab4a6fa8fa90
Letícia Ferreira Pinto <103539215+L3ticialele@users.noreply.github.com>
2023-10-30 18:06:30 -0300

    Add files via upload

---
+++ Altomare/idao/src/main/java/br/cefetmg/altomare/model/dao/IFuncionarioDAO.java
@@ -0,0 +1,15 @@
+
+package br.cefetmg.altomare.model.dao;
+
+import br.cefetmg.altomare.model.dto.FuncionarioDTO;
+import br.cefetmg.altomare.model.dao.exception.PersistenciaException;
+import java.util.ArrayList;
+
+public interface IFuncionarioDAO{
+    long inserir(FuncionarioDTO funcionario) throws PersistenciaException;
+    boolean atualizar(FuncionarioDTO funcionario) throws PersistenciaException;
+    boolean delete(FuncionarioDTO funcionario) throws PersistenciaException;
+    ArrayList<FuncionarioDTO> listarTodos() throws PersistenciaException;
+    FuncionarioDTO consultarPorId(long cpf) throws PersistenciaException;
+    FuncionarioDTO consultarPorUsuarioSenha(String cpf, String senha) throws PersistenciaException;
+}
