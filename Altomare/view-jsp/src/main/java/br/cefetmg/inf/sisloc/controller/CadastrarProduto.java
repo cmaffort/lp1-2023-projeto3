3d0c0914c7eb15c4fbcbbc794fc1f6402a978ac6
Matheus Amaral de Assis <127154753+Matheus-deAssis@users.noreply.github.com>
2023-10-31 05:04:26 -0300

    Create CadastrarProduto.java

---
+++ Altomare/view-jsp/src/main/java/br/cefetmg/inf/sisloc/controller/CadastrarProduto.java
@@ -0,0 +1,47 @@
+package controller;
+import br.cefetmg.altomare.dao.mysql.ProdutoDAO;
+import br.cefetmg.altomare.dto.ProdutoDTO;
+import java.io.IOException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Date;
+import java.sql.SQLException;
+
+@WebServlet("/CadastrarProduto")
+public class CadastrarProduto extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public static String execute(HttpServletRequest request) {
+        String jsp = "";
+
+        try {
+            String nome = request.getParameter("NAMEProd");
+            String precoStr = request.getParameter("PrecProd");
+            String estado = request.getParameter("EstadPro");
+            String tipo = request.getParameter("tipoProduto");
+            String quantidadeStr = request.getParameter("quantidade");
+            String dataStr = request.getParameter("data");
+
+            double preco = Double.parseDouble(precoStr);
+            int quantidade = Integer.parseInt(quantidadeStr);
+            Date data = Date.valueOf(dataStr);
+
+            ProdutoDTO produto = new ProdutoDTO(nome, preco, tipo, estado, null, quantidade, data);
+
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+            produtoDAO.inserirProduto(produto);
+
+            jsp = "index.jsp"; 
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            jsp = "headerprodutos.jsp"; 
+        }
+
+        return jsp;
+    }
+}
