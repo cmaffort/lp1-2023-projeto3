c56515d47b5eb6c145aa056915744dd1a3ced6f7
Matheus Amaral de Assis <127154753+Matheus-deAssis@users.noreply.github.com>
2023-10-31 05:10:39 -0300

    Update Estoque.java

--- Altomare/dto/src/main/java/br/cefetmg/altomare/model/dto/Estoque.java
+++ Altomare/dto/src/main/java/br/cefetmg/altomare/model/dto/Estoque.java
@@ -3,15 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package br.cefetmg.altomare.dto;
-import br.cefetmg.altomare.dto.Produto;
 
-/**
- *
- * @author User
- */
-public class Estoque {
-    
-     private Produto produto;
-    
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstoqueDTO {
+    private List<ProdutoDTO> produtos;
+
+    public EstoqueDTO() {
+        produtos = new ArrayList<>();
+    }
+
+    public List<ProdutoDTO> getProdutos() {
+        return produtos;
+    }
+
+    /*public void adicionarProduto(ProdutoDTO produto) {
+        for (ProdutoDTO p : produtos) {
+            if (p.getId().equals(produto.getId())) {
+                p.adicionarQuantidade(produto.getQuantidade());
+                return;
+            }
+        }
+        produtos.add(produto);
+    }
+*/
+    public void removerProduto(ProdutoDTO produto) {
+        produtos.remove(produto);
+    }
 }
