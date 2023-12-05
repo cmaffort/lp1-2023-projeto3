6c54433cc4621780d367f2386bdd0d6794c87060
Letícia Ferreira Pinto <103539215+L3ticialele@users.noreply.github.com>
2023-12-04 23:19:22 -0300

    Update Fachada.java

--- Altomare/view-jsp/src/main/java/br/cefetmg/inf/altomare/controller/Fachada.java
+++ Altomare/view-jsp/src/main/java/br/cefetmg/inf/altomare/controller/Fachada.java
@@ -1,41 +1,119 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Cadastro de Atividade | AltoMare</title>
-                <link rel="stylesheet" type="text/css" href="../../css/produtos.css">
 
-    </head>
-    <body>
-        <%@ include file="../../headerGerente.jsp" %>
-        <h1>Cadastro de Nova Atividade</h1>
-        <form action="../../CadastrarAtividadeServlet" method="post">
-            <label>Data:</label>
-            <input type="text" name="data" required><br>
+package br.cefetmg.inf.altomare.controller;
 
-            <label>Hora de Início:</label>
-            <input type="text" name="horaInicio" required><br>
+import br.cefetmg.altomare.model.dao.exception.PersistenciaException;
+import br.cefetmg.altomare.model.exception.NegocioException;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//@include file="../../headerGerente.jsp";
 
-            <label>Hora de Término:</label>
-            <input type="text" name="horaTermino" required><br>
+@WebServlet(name = "Fachada", urlPatterns = {"/Fachada"})
+@MultipartConfig
+public class Fachada extends HttpServlet {
 
-            <label>Local:</label>
-            <input type="text" name="local" required><br>
-
-            <label>Limite de Ocupação:</label>
-            <input type="number" name="limiteOcupacao" required><br>
-
-            <label>Responsável:</label>
-            <input type="text" name="responsavel" required><br>
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+           throws ServletException, IOException, PersistenciaException, NegocioException, SQLException, ClassNotFoundException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try {
+            request.setCharacterEncoding("UTF-8");
+           String acao = request.getParameter("acao");
+           String jsp ;
+           
+           //Part filePart = request.getPart("foto"); // Obtém o upload do arquivo
+            //String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // Obtém o nome do arquivo
+            //InputStream fileContent = filePart.getInputStream(); // Obtém o conteúdo do arquivo
             
-            <label>Descrição</label>
-            <input type="text" name="descricao" required><br>
-            
-            <label>Visivel? (S/N)</label>
-            <input type="text" name="visivel" pattern="(S|N)" required><br>
+            //Armazena o InputStream em um atributo de solicitação para que ele possa ser acessado na servlet CadastrarPassageiro
+          // request.setAttribute("foto", fileContent);
 
-            <input type="submit" name="cadastrar" value="Cadastrar">
-        </form>
-    </body>
-</html>
+       if(acao.equals("Login"))
+          jsp = Login.execute(request);   
+       else if(acao.equals("CadastrarPassageiro"))
+           jsp = CadastrarPassageiro.execute(request);
+       else if(acao.equals("CadastrarItem"))
+           jsp = Cardapio.execute(request);
+       else if(acao.equals("CadastrarFuncionario"))
+           jsp = CadastrarFuncionario.execute(request);
+       else if(acao.equals("CadastrarSeguranca"))
+           jsp = CadastrarSeguranca.execute(request);
+       else if(acao.equals("ExcluirSeguranca"))
+           jsp = ExcluiSeguranca.execute(request);
+       else
+           jsp = "/erro.jsp";
+       
+       //Redirecionando pagina
+        response.sendRedirect(jsp);
+        
+        }
+        catch(Exception e) {
+                 e.printStackTrace();
+        }
+    }
+        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         try {
+             processRequest(request, response);
+         } catch (PersistenciaException | NegocioException ex) {
+         } catch (SQLException ex) {
+            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         try {
+             processRequest(request, response);
+         } catch (PersistenciaException | NegocioException ex) {
+         } catch (SQLException ex) {
+            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
+    
