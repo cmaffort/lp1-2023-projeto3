5c23b38747ee573eed6cb7225fefa50bd6e37b94
Letícia Ferreira Pinto <103539215+L3ticialele@users.noreply.github.com>
2023-12-04 23:17:15 -0300

    Update Fachada.java

--- Altomare/view-jsp/src/main/java/br/cefetmg/inf/altomare/controller/Fachada.java
+++ Altomare/view-jsp/src/main/java/br/cefetmg/inf/altomare/controller/Fachada.java
@@ -1,109 +1,41 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cadastro de Atividade | AltoMare</title>
+                <link rel="stylesheet" type="text/css" href="../../css/produtos.css">
 
-package br.cefetmg.inf.altomare.controller;
+    </head>
+    <body>
+        <%@ include file="../../headerGerente.jsp" %>
+        <h1>Cadastro de Nova Atividade</h1>
+        <form action="../../CadastrarAtividadeServlet" method="post">
+            <label>Data:</label>
+            <input type="text" name="data" required><br>
 
-import br.cefetmg.altomare.model.dao.exception.PersistenciaException;
-import br.cefetmg.altomare.model.exception.NegocioException;
-import jakarta.servlet.RequestDispatcher;
-import java.io.IOException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-//@include file="../../headerGerente.jsp";
+            <label>Hora de Início:</label>
+            <input type="text" name="horaInicio" required><br>
 
-@WebServlet(name = "Fachada", urlPatterns = {"/Fachada"})
-public class Fachada extends HttpServlet {
+            <label>Hora de Término:</label>
+            <input type="text" name="horaTermino" required><br>
 
-    @SuppressWarnings("ConvertToStringSwitch")
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException, PersistenciaException, NegocioException, SQLException, ClassNotFoundException, ClassNotFoundException {
-        response.setContentType("text/html;charset=UTF-8");
-        
-        try ( PrintWriter out = response.getWriter()) {
-            request.setCharacterEncoding("UTF-8");
-           String acao = request.getParameter("acao");
-           String jsp ;
-           
-       // out.println("<head>");
-         // out.println("<link rel=\"stylesheet\" href=\"../../css/estilos-header.css\">");
-          //out.println("</head>");
-        
+            <label>Local:</label>
+            <input type="text" name="local" required><br>
 
-       if(acao.equals("Login")){
-          jsp = Login.execute(request);  
-          response.sendRedirect(jsp);
-       }
-       else if(acao.equals("CadastrarPassageiro")){
-           jsp = CadastrarPassageiro.execute(request);
-           response.sendRedirect(jsp);
-       }
-       else if(acao.equals("CadastrarItem")){
-           jsp = Cardapio.execute(request);
-           response.sendRedirect(jsp);
-       }
-       else if(acao.equals("CadastrarFuncionario")){
-           jsp = CadastrarFuncionario.execute(request);
-           response.sendRedirect(jsp);
-       }
-       else{
-           jsp = "/erro.jsp";
-           response.sendRedirect(jsp);
-       }
-       
-        }
-    }
-        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-         try {
-             processRequest(request, response);
-         } catch (PersistenciaException | NegocioException ex) {
-         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+            <label>Limite de Ocupação:</label>
+            <input type="number" name="limiteOcupacao" required><br>
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-         try {
-             processRequest(request, response);
-         } catch (PersistenciaException | NegocioException ex) {
-         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(Fachada.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+            <label>Responsável:</label>
+            <input type="text" name="responsavel" required><br>
+            
+            <label>Descrição</label>
+            <input type="text" name="descricao" required><br>
+            
+            <label>Visivel? (S/N)</label>
+            <input type="text" name="visivel" pattern="(S|N)" required><br>
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-}
-    
+            <input type="submit" name="cadastrar" value="Cadastrar">
+        </form>
+    </body>
+</html>
